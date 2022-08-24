377e16922972730aa3cd7ee6343cbbf0a5994307
Subham Panda <iamsubhampanda7@gmail.com>
2022-08-24 12:25:34 +0530

    navbar added

---
+++ navbar.js
@@ -0,0 +1,34 @@
+function navbar() {
+  return `<div id="navbar">
+    <div id="childnavbar">
+      <div>
+        <a href="./index.html">
+          <img
+            src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg"
+            alt=""
+        /></a>
+      </div>
+      <div></div>
+      <div>
+        <a href=""><p>How it works</p> </a>
+      </div>
+      <div>
+        <a href=""><p>Use cases</p></a>
+      </div>
+      <div>
+        <a href=""><p>Pricing</p></a>
+      </div>
+      <div>
+        <a href=""><p>Support</p></a>
+      </div>
+      <div>
+        <a href=""><p>Sign in</p></a>
+      </div>
+      <div>
+        <button><a href="">Get My Hours Free</a></button>
+      </div>
+    </div>
+  </div>`;
+}
+
+export default navbar
