5f14f7d70b3e245079c9bf441bcb5b3d9c463a75
saima2310 <saima.zaki2310@gmail.com>
2022-08-25 18:13:26 +0530

    login added

--- signup.js
+++ signup.js
@@ -3,14 +3,21 @@ document.querySelector('#btn').addEventListener('click', signUp)
     function signUp  (event) {
     event.preventDefault()
     document.querySelector("#warn1").textContent = "";
+    document.querySelector("#warn2").textContent = "";
+    document.querySelector("#warn3").textContent = "";
 
     let name=document.getElementById('name').value
     let email=document.getElementById('email').value
     let password=document.getElementById('password').value
-
+        console.log(name,email,password)
     if(password.length<6){
-        document.getElementById('warn1').innerText="Password must be 6 characters long."
+        document.getElementById('warn1').innerText="Password must at least be 6 characters long."
         return 0;
+    }else if((name.length<2) && (email<2)){
+
+        document.getElementById('warn3').innerText="Please enter full name"
+        document.getElementById('warn2').innerText="Please enter valid email"
+        return 0
     }else{
         var obj = {
             Name : name,
@@ -22,6 +29,9 @@ document.querySelector('#btn').addEventListener('click', signUp)
         localStorage.setItem('user', JSON.stringify(obj))
     }
     console.log(obj)
+    name=document.getElementById('name').value=null
+    email=document.getElementById('email').value=null
+    password=document.getElementById('password').value=null
     alert('signup successful')
     window.location.href="login.html"
  }
@@ -34,3 +44,7 @@ document.querySelector('#btn').addEventListener('click', signUp)
 //         this.password=p;
 //     }
 // }
+
+document.getElementById('image').addEventListener('click', function(){
+    window.location.href="index.html"
+})
