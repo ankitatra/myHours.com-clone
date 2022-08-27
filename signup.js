e9ec0dd11bdb7ea36507820b417c71624f57b192
saima2310 <saima.zaki2310@gmail.com>
2022-08-27 16:57:05 +0530

    added

--- signup.js
+++ signup.js
@@ -3,8 +3,8 @@ document.querySelector('#btn').addEventListener('click', signUp)
     function signUp  (event) {
     event.preventDefault()
     document.querySelector("#warn1").textContent = "";
-    document.querySelector("#warn2").textContent = "";
-    document.querySelector("#warn3").textContent = "";
+    // document.querySelector("#warn2").textContent = "";
+    // document.querySelector("#warn3").textContent = "";
 
     let name=document.getElementById('name').value
     let email=document.getElementById('email').value
@@ -13,11 +13,15 @@ document.querySelector('#btn').addEventListener('click', signUp)
     if(password.length<6){
         document.getElementById('warn1').innerText="Password must at least be 6 characters long."
         return 0;
-    }else if((name.length<2) && (email<2)){
-
+    }else if(
+       ( document.getElementById('name').value='') ||
+       ( document.getElementById('email').value='' )||
+        (document.getElementById('password').value='')
+    ){
+        alert("Please enter all the details")
         document.getElementById('warn3').innerText="Please enter full name"
         document.getElementById('warn2').innerText="Please enter valid email"
-        return 0
+        
     }else{
         var obj = {
             Name : name,
